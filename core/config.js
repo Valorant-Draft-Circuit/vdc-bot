08bd6e7200f711375899a8ed3deb24692a06619d
Rohan Gupta <75713762+torohangupta@users.noreply.github.com>
2023-09-01 10:34:22 -0500

    Gutted config.js in prep for changes

--- core/config.js
+++ core/config.js
@@ -1,68 +1,9 @@
 module.exports = {
-    OWNER_ID: `382893405178691584`,
-    WALLE_ID: `863547321630720050`,
-    PREFIX: `~`, /** @deprecated */
-
-    SERVER_ID: {
-        ONLINE_COLLEGE: `692094440881520671`,
-        DEVELOPMENT: `740038736561307678`
-    },
-
     PRESENCE: {
         STATUS: `online`,
         TYPE: `WATCHING`,
         MESSAGE: `rohan write bad code`,
     },
 
-    GITHUB : `https://github.com/torohangupta/Wall-E`,
-
-    IMAGE: {
-        SQUARE: `https://unitedtheme.com/live-preview/starter-gazette/wp-content/uploads/2018/12/image-005-720x720.jpg`,
-    },
-    
-    /* Bot Embed Colors */
-    EMBED_COLORS: {
-        BOT_EMBED: `EB9850`,
-        TRANSPARENT: `36393F`,
-
-        SUCCESS: `00A56A`,
-        ERROR: `D61A3C`,
-        WARNING: `F7E919`,
-        AUTOMOD: `36393F`,
-
-        TICKET_CREATE: `068ADD`,
-        TICKET_CLOSE: `068ADD`,
-
-        KICK_LOG: `FF7961`,
-        SOFTBAN_LOG: `AF4448`,
-        BAN_LOG: `D32F2F`,
-
-        LOG_DMS: `507F7F`,
-    },
-    
-    MESSAGES: {
-        API_ERROR: `Unexpected Backend Error! Try again later or contact support server`,
-    },
-
-    NEW_VOICE_CHANNEL_BUTTON: `1041441019612377169`,
-
-    CHANNELS: {
-        GETTING_STARTED: `984572699173089290`,
-        CONTACT_MODS: `866434475495129118`,
-        CONSOLE: `768856507903115294`,
-        UPDATES: `801893303444045854`,
-        LOG_DMS: `787830182781255681`,
-        LOG_AUTOMOD: `1057348284961796096`,
-        LOG_SUPPORT: `789907442582028308`,
-        LOG_MODAPPS: `933239367818944592`,
-        MOD_PRIVATE: `790712186749321226`,
-    },
-
-    ROLES: {
-        ADMIN: `694651259059437710`,
-        MOD: `692097359005351947`,
-        TRAINING_MOD: `933375701099372604`,
-        BOT: `692100602297188382`,
-        STUDENTS: `692097738283810896`,
-    }
+    GITHUB : `https://github.com/Unieveth/vdc-bot`,
 };
