2f2931369ec4d590fc616293f8780be78b845687
Rohan Gupta <75713762+torohangupta@users.noreply.github.com>
2023-10-16 13:46:04 -0500

    removed js indef files (will be compiled at deployment)

--- prisma/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Team = exports.Player = exports.Franchise = void 0;
-var _Franchise_1 = require("./_Franchise");
-Object.defineProperty(exports, "Franchise", { enumerable: true, get: function () { return _Franchise_1.Franchise; } });
-var _Player_1 = require("./_Player");
-Object.defineProperty(exports, "Player", { enumerable: true, get: function () { return _Player_1.Player; } });
-var _Team_1 = require("./_Team");
-Object.defineProperty(exports, "Team", { enumerable: true, get: function () { return _Team_1.Team; } });
