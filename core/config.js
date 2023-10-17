ad70e47b9c0f9eeaa4032e479939b774401b8694
Rohan Gupta <75713762+torohangupta@users.noreply.github.com>
2023-10-17 14:23:22 -0500

    Fixed presense code in v14(?) changes

--- core/config.js
+++ core/config.js
@@ -1,8 +1,9 @@
+const { ActivityType } = require("discord.js");
+
 module.exports = {
     PRESENCE: {
-        STATUS: `online`,
-        TYPE: `WATCHING`,
-        MESSAGE: `rohan write bad code`,
+        TYPE: ActivityType.Watching,
+        MESSAGE: `travestey write bad code`,
     },
 
     GITHUB : `https://github.com/Unieveth/vdc-bot`,
