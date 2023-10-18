056cd9a7590052c00100bd4af7031eaab862993d
Rohan Gupta <75713762+torohangupta@users.noreply.github.com>
2023-10-18 11:59:40 -0500

    Deleted, complied via command line in package.json script

--- utils/enums/compile.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# MACOS SHELL SCRIPT - THIS ONLY WORKS ON UNIX BASED SYSTEMS
-# This shell script compiles all TypeScript files in the current directory.
-
-for tsfile in ./utils/enums/*.ts; do
-  tsc $tsfile
-done
