a6242b998b6e9880ecdeac3c6c33954834c676de
Rohan Gupta <75713762+torohangupta@users.noreply.github.com>
2023-10-19 02:03:42 -0500

    Fixed reference errors & updated to more accurately reflect proper syntax

--- interactions/commands/welcome.js
+++ interactions/commands/welcome.js
@@ -1,5 +1,5 @@
 const { Player } = require("../../prisma");
-const { Channel, Roles } = require(`../../utils/enums`);
+const { CHANNELS, ROLES } = require(`../../utils/enums`);
 
 module.exports = {
 
@@ -12,11 +12,11 @@ module.exports = {
         const status = _hoistedOptions[1].value;
 
         const guildMember = await interaction.guild.members.fetch(player.value);
-        const acceptedChannel = await interaction.guild.channels.fetch(Channel.ACCEPTED_MEMBERS);
+        const acceptedChannel = await interaction.guild.channels.fetch(CHANNELS.ACCEPTED_MEMBERS);
 
         // renove the viewer role & add the league role
-        if (guildMember._roles.includes(Roles.LEAGUE.VIEWER)) await guildMember.roles.remove(Roles.LEAGUE.VIEWER);
-        await guildMember.roles.add(Roles.LEAGUE.LEAGUE);
+        if (guildMember._roles.includes(ROLES.LEAGUE.VIEWER)) await guildMember.roles.remove(ROLES.LEAGUE.VIEWER);
+        await guildMember.roles.add(ROLES.LEAGUE.LEAGUE);
 
         // update the name to match convention
         const ign = (await Player.getIGNby({ discordID: player.value })).split(`#`)[0];
@@ -25,11 +25,11 @@ module.exports = {
         // assign the proper roles & send the correct message
         switch (status) {
             case `DE`:
-                await guildMember.roles.add(Roles.LEAGUE.DRAFT_ELIGIBLE);
+                await guildMember.roles.add(ROLES.LEAGUE.DRAFT_ELIGIBLE);
                 acceptedChannel.send({ content: `Welcome ${player.user} to the league as a DE!` });
                 break;
             case `RFA`:
-                await guildMember.roles.add(Roles.LEAGUE.RESTRICTED_FREE_AGENT);
+                await guildMember.roles.add(ROLES.LEAGUE.RESTRICTED_FREE_AGENT);
                 acceptedChannel.send({ content: `Welcome ${player.user} to the league as an RFA!` });
                 break;
             default:
