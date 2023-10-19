cd874664b0b9c3ce21b79c9390d85f856ea8b958
Rohan Gupta <75713762+torohangupta@users.noreply.github.com>
2023-10-19 01:53:52 -0500

    More enum updates

--- utils/enums/channels.ts
+++ utils/enums/channels.ts
@@ -1,3 +1,14 @@
 export enum Channel {
-    TRANSACTIONS = "963553611051319316",
 }
+
+export const CHANNELS = process.env.ENVIRONMENT == "DEV" ?
+    /** @development */
+    {
+        TRANSACTIONS: "1057088094312091678",
+        ACCEPTED_MEMBERS: "1057088139711234078",
+    } :
+    /** @production */
+    {
+        TRANSACTIONS: "963553611051319316",
+        ACCEPTED_MEMBERS: "1049259805379924058",
+    };
