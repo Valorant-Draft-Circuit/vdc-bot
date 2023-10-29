a646259d110ada1d1bb9b5457f40bc018a35cd60
Rohan Gupta <75713762+torohangupta@users.noreply.github.com>
2023-10-29 00:48:15 -0500

    Added `transactions` to the whitelist

--- core/botClient.js
+++ core/botClient.js
@@ -10,7 +10,7 @@ const {
 const { Channel } = require("discord.js").Partials;
 
 /** @NOTE - THIS IS TEMPORARY. WILL BE CHANGED ASAP */
-const cmdWhitelist = [`ping`, `submit`, `topic`, `welcome`];
+const cmdWhitelist = [`ping`, `submit`, `topic`, `welcome`, `transactions`];
 
 
 module.exports = class BotClient extends Client {
