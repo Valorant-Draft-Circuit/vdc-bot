0da2fb17b1dcf157a5f46dbe4a6796a4f1b7d768
Rohan Gupta <75713762+torohangupta@users.noreply.github.com>
2023-11-10 13:42:02 -0600

    Created commands for the Activity Check (#20)
    * Removed Franchise enums
    
    Franchise roles are now stored in the database, this is deprecated
    
    * Added file for ButtonOptions enums
    
    This will replace `./transactions.ts` for button types in the long term
    
    * Added `Inactive` role ID in dev/prod
    
    There is a known issue that needs to be investigated, the IR & Inactive role IDs are identical at the present moment. Need to determine the correct role ID for IR (if it exists)
    
    * Create `/setup` command
    
    - Added `activity-check` as a subcommand with options to add more setup style commands in the future.
    
    * Created the `/active` command
    
    For users to run activity check
    
    * Confirm & cancel button processing for `activity-check` subcommand from `/setup`
    
    * Made activity check messages ephemeral
    
    Also added an else statement with a special second message
    
    * travestey is dummy
    
    * Set API query frequency to 50ms/call

--- utils/enums/roles.ts
+++ utils/enums/roles.ts
@@ -7,38 +7,6 @@
  */
 
 /**
- * @enum {Franchise} Enumerations for the franchise roles
- * @param {String} IE Ignium Esports
- * @param {String} VTX Infinite Vortex
- * @param {String} KC Kingdom Coffee
- * @param {String} LG Light Garden
- * @param {String} L7 Lucky Sevens
- * @param {String} OS Omnipotent Sandwiches
- * @param {String} TT DoubleTaps
- * @param {String} ATO The Automata
- * @param {String} PA Party Animals
- * @param {String} MF The Mafia
- * @param {String} SOL Solaris
- * @param {String} PPI Post Plant Incorporated 
- */
-
-export enum Franchise {
-    IE = "969642817397850112",
-    VTX = "970712837003563048",
-    KC = "971546196407635999",
-    LG = "968755818231128114",
-    L7 = "966395530211127396",
-    OS = "968018582934024262",
-    TT = "966904182223290488",
-    ATO = "963905820351946764",
-    PA = "963905754711076906",
-    MF = "971546122160062474",
-    SOL = "970713326709514250",
-    PPI = "966904250615599163",
-}
-
-
-/**
  * @enum {Status} The player's Status
  * @param {String} DRAFT_ELIGIBLE Role ID for the Draft Eligible Status
  * @param {String} FREE_AGENT Role ID for the Free Agent Status
@@ -80,10 +48,11 @@ export const ROLES = process.env.ENVIRONMENT == "DEV" ?
         LEAGUE: {
             VIEWER: "1062105398108225556",
             LEAGUE: "1057161314201518091",
+            INACTIVE: "1060751264088080445",
             DRAFT_ELIGIBLE: "1057161198702952508",
             FREE_AGENT: "1057161311651377183",
             RESTRICTED_FREE_AGENT: "1057163956524949534",
-            INACTIVE_RESERVE: "1060751264088080445",
+            INACTIVE_RESERVE: "1060751264088080445", /** @TODO - INVESTIGATE: THIS ROLE ID IS WRONG */
         },
         TIER: {
             MYTHIC: "1057161199659257928",
@@ -97,10 +66,11 @@ export const ROLES = process.env.ENVIRONMENT == "DEV" ?
         LEAGUE: {
             VIEWER: "963665382873391135",
             LEAGUE: "966901006652833862",
+            INACTIVE: "1060750208746668132",
             DRAFT_ELIGIBLE: "963568945959419905",
             FREE_AGENT: "963343035683455007",
             RESTRICTED_FREE_AGENT: "965817407094530068",
-            INACTIVE_RESERVE: "1060750208746668132",
+            INACTIVE_RESERVE: "1060750208746668132", /** @TODO - INVESTIGATE: THIS ROLE ID IS WRONG */
         },
         TIER: {
             MYTHIC: "967698140612395018",
