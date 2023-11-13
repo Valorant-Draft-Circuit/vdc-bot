7bb677690f5747854e0cf85741557d5675d9cfbc
Rohan Gupta <75713762+torohangupta@users.noreply.github.com>
2023-11-12 22:50:35 -0600

    Feature activity check warning reports (#22)
    * Fun fact, if you try to get null from an object, it doesn't work!
    
    * Added debugging to the `activity-check`
    
    Sure it's late and after the check has begun but you know what they say (i don't, please tell me)

--- interactions/commands/setup.js
+++ interactions/commands/setup.js
@@ -1,8 +1,10 @@
-const { EmbedBuilder, ActionRowBuilder, ButtonBuilder } = require("discord.js");
+const { EmbedBuilder, ActionRowBuilder, ButtonBuilder, StringSelectMenuBuilder } = require("discord.js");
 const { ButtonStyle } = require(`discord.js`)
 
 const { Player } = require("../../prisma");
 const { ROLES, ButtonOptions } = require("../../utils/enums");
+
+const MS_PER_API_CALL = 50; // PREDEFINED API REQUESTS RATE (MAX RATE IS 1 CALL EVERY 20 MS)
 
 module.exports = {
 
@@ -32,7 +34,7 @@ async function activityCheck(interaction) {
     const leagueRole = await interaction.guild.roles.fetch(ROLES.LEAGUE.LEAGUE);
     const leagueRoleMemberIDs = await leagueRole.members.map(m => m.id);
 
-    // get all guild members with the League role & store their ID
+    // get all guild members with the Inactive role & store their ID
     const inactiveRole = await interaction.guild.roles.fetch(ROLES.LEAGUE.INACTIVE);
     const inactiveRoleMemberIDs = await inactiveRole.members.map(m => m.id);
 
@@ -45,17 +47,23 @@ async function activityCheck(interaction) {
     // determine potential warnings/discrepancies
     const inLeagueNotPlayerTable = leagueRoleMemberIDs.filter(id => !allActivePlayerIDs.includes(id));
     const inPlayerTableNotLeague = allActivePlayerIDs.filter(id => !leagueRoleMemberIDs.includes(id));
+    const inLeagueANDInactiveANDInPlayerTable = leagueRoleMemberIDs
+        .filter((id) => allActivePlayerIDs.includes(id))
+        .filter((id) => inactiveRoleMemberIDs.includes(id));
 
     // create the warnings portion of the embed message
     const warningMessage = `**Warning(s)**\n` +
         `\` ${String(inLeagueNotPlayerTable.length).padStart(3, ` `)} \` : # of users w/ <@&${ROLES.LEAGUE.LEAGUE}> role & NOT in database\n` +
         `\` ${String(inPlayerTableNotLeague.length).padStart(3, ` `)} \` : # of users w/o <@&${ROLES.LEAGUE.LEAGUE}> role but ARE in database\n` +
-        `\` ${String(inactiveRoleMemberIDs.length).padStart(3, ` `)} \` : # of users who already have the <@&${ROLES.LEAGUE.INACTIVE}> role\n`;
+        `\` ${String(inactiveRoleMemberIDs.length).padStart(3, ` `)} \` : # of users who already have the <@&${ROLES.LEAGUE.INACTIVE}> role\n` +
+        `\` ${String(inLeagueANDInactiveANDInPlayerTable.length).padStart(3, ` `)} \` : # of users w/ <@&${ROLES.LEAGUE.LEAGUE}> & <@&${ROLES.LEAGUE.INACTIVE}> roles & ARE in database\n`;
+
+    const expectedRuntime = Math.round((MS_PER_API_CALL * sharedMemberIDs.length / 10)) / 100;
 
     // create the base embed
     const embed = new EmbedBuilder({
         author: { name: `VDC Activity Check` },
-        description: `__Operation Overview:__\nAre you certain you want to begin the activity check? This will give ${sharedMemberIDs.length} member(s) the <@&${ROLES.LEAGUE.INACTIVE}> role.`,
+        description: `__Operation Overview:__\nAre you certain you want to begin the activity check? This will give ${sharedMemberIDs.length} member(s) the <@&${ROLES.LEAGUE.INACTIVE}> role. This operation should take approximately ${expectedRuntime} second(s).`,
         color: 0xE92929,
         fields: [{ name: `\u200B`, value: warningMessage, }],
         footer: { text: `Setup — Activity Check` }
@@ -74,7 +82,36 @@ async function activityCheck(interaction) {
         style: ButtonStyle.Success,
     });
 
+    const selectMenu = new StringSelectMenuBuilder({
+        customId: `activityCheckDebug`,
+        placeholder: 'Optional/Debug: Select reports to generate.',
+        maxValues: 5,
+        options: [
+            {
+                label: `Users who will recieve the Inactive Role`,
+                value: `0`,
+            },
+            {
+                label: `Users w/ League role & NOT in database`,
+                value: `1`,
+            },
+            {
+                label: `Users w/o League role but ARE in database`,
+                value: `2`,
+            },
+            {
+                label: `Users who already have the Inactive role`,
+                value: `3`,
+            },
+            {
+                label: `Users WITH the League & Inactive roles & ARE in the database`,
+                value: `4`,
+            }
+        ]
+    });
+
     // create the action row, add the component to it & then reply with all the data
-    const subrow = new ActionRowBuilder({ components: [cancel, confirm] });
-    return await interaction.editReply({ embeds: [embed], components: [subrow] });
+    const row1 = new ActionRowBuilder({ components: [selectMenu] });
+    const row2 = new ActionRowBuilder({ components: [cancel, confirm] });
+    return await interaction.editReply({ embeds: [embed], components: [row1, row2] });
 }
