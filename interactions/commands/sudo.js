7a37faee64d9da88d2b84440af6e3157cbe42d50
Rohan Gupta <75713762+torohangupta@users.noreply.github.com>
2023-11-18 10:31:23 -0600

    Added output for error messages

--- interactions/commands/sudo.js
+++ interactions/commands/sudo.js
@@ -25,9 +25,18 @@ module.exports = {
       if (mmr) updateParamaters.MMR = mmr.value;
 
       const playerBefore = await Player.getBy({ discordID: user.id });
-      const playerAfter = await Player.updateBy({ userIdentifier: { discordID: user.id }, updateParamaters: updateParamaters });
+      const playerAfter = await Player.updateBy({ userIdentifier: { discordID: user.id }, updateParamaters: updateParamaters }).catch(e => e);
 
-      console.log(playerAfter)
+      if (playerAfter instanceof Error) {
+         const errorEmbed = new EmbedBuilder({
+            author: { name: `ERROR : Player Database Update` },
+            description: `The database was not updated. The error is captured below:\`\`\`js\n${playerAfter.stack}\n\`\`\``,
+            color: 0xE92929,
+            footer: { text: `Sudo — Player: Update` }
+         });
+         return await interaction.editReply({ embeds: [errorEmbed] });
+      }
+
       // create the base embed
       const embed = new EmbedBuilder({
          author: { name: `Player Database Update` },
