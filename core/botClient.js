53d20b425b206129ed304933dc997801f8ce278c
Sooho <95513590+dlatngh@users.noreply.github.com>
2023-11-28 10:39:17 -0800

    Feature - Profile: Update Command (#41)
    * added update profile
    
    * updateRiotID parameter takes in Object
    
    * am i stupid? updated the 2nd updateRiotID call...
    
    * formatting
    
    * formatting
    
    * Update to reflect more accurate function name
    
    * Refactor/rewrite
    
    * Consolidated the desynced database & nickname into one return
    
    There's no penalty for updating a user's nickname and same for the database, so if both of those are not fulfilled, the bot will try to update the database and the user's nickname
    
    * Well... this one's self explanatory
    
    Silly dLa going around and committing files named `dontCommitThis.js`
    
    * removed package-lock.json & added  to `.gitignore`
    
    * Update to `/profile update` description
    
    * can't forget the comma!
    
    * Correctly updates status
    
    * Cleared dead/unused/depricated code from `Player` static class
    
    * Update slug from database to reflect FA/RFA status
    
    ---------
    
    Co-authored-by: Rohan Gupta <75713762+torohangupta@users.noreply.github.com>

--- core/botClient.js
+++ core/botClient.js
@@ -10,7 +10,7 @@ const {
 const { Channel } = require("discord.js").Partials;
 
 /** @NOTE - THIS IS TEMPORARY. WILL BE CHANGED ASAP */
-const cmdWhitelist = [`ping`, `submit`, `topic`, `welcome`, `transactions`, `setup`, `active`, `roster`, `sudo`, `sub`];
+const cmdWhitelist = [`ping`, `submit`, `topic`, `welcome`, `transactions`, `setup`, `active`, `roster`, `sudo`, `profile`, `sub`];
 
 
 module.exports = class BotClient extends Client {
