721344695b6802e73d5f2a68ab53ff6a5ee91c38
Rohan Gupta <75713762+torohangupta@users.noreply.github.com>
2023-12-05 10:47:10 -0600

    Query the database

--- prisma/_Games.ts
+++ prisma/_Games.ts
@@ -15,13 +15,27 @@ export class Games {
                 rounds_played: 1000
             },
         })
-    }
+    };
 
     static async exists(options: { id: string }) {
+        const { id } = options;
+        if (!/([a-z0-9]{8})-([a-z0-9]{4}-){3}([a-z0-9]{12})$/.test(id)) throw new Error(`Invalid Match ID!`);
+        
         const match = await prisma.games.findUnique({
-            where: { id: options.id }
+            where: { id: id }
         })
 
         return match === null ? false : true;
-    }
+    };
+
+    static async getMatchData(options: { id: string }) {
+        const { id } = options;
+
+        if (!/([a-z0-9]{8})-([a-z0-9]{4}-){3}([a-z0-9]{12})$/.test(id)) throw new Error(`Invalid Match ID!`);
+        return await prisma.games.findFirst({
+            where: { id: id },
+            include: { PlayerStats: { include: { Player: { include: { Account: true, Team: true } } } } }
+        });
+
+    };
 }
