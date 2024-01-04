2653585d7e1e7ef3f2b51ab20d5644e7b1914a68
Rohan Gupta <75713762+torohangupta@users.noreply.github.com>
2024-01-04 16:05:34 -0600

    Add enums & left justify values into a seperate column

--- utils/enums/roles.ts
+++ utils/enums/roles.ts
@@ -46,40 +46,48 @@ export const ROLES = process.env.ENVIRONMENT == "DEV" ?
     /** @development */
     {
         LEAGUE: {
-            VIEWER: "1062105398108225556",
-            LEAGUE: "1057161314201518091",
-            INACTIVE: "1060751264088080445",
-            DRAFT_ELIGIBLE: "1057161198702952508",
-            FREE_AGENT: "1057161311651377183",
-            RESTRICTED_FREE_AGENT: "1057163956524949534",
-            INACTIVE_RESERVE: "1181457461895495732",
-            CAPTAIN: "1082176642329346110",
-            FORMER_PLAYER: "1062105226225660005",
+            VIEWER                  : "1062105398108225556",
+            LEAGUE                  : "1057161314201518091",
+            INACTIVE                : "1060751264088080445",
+            DRAFT_ELIGIBLE          : "1057161198702952508",
+            FREE_AGENT              : "1057161311651377183",
+            RESTRICTED_FREE_AGENT   : "1057163956524949534",
+            INACTIVE_RESERVE        : "1181457461895495732",
+            CAPTAIN                 : "1082176642329346110",
+            FORMER_PLAYER           : "1062105226225660005",
         },
         TIER: {
-            MYTHIC: "1057161199659257928",
-            EXPERT: "1057161297319440485",
-            APPRENTICE: "1057161301102698567",
-            PROSPECT: "1057161308535005204",
+            MYTHIC                  : "1057161199659257928",
+            EXPERT                  : "1057161297319440485",
+            APPRENTICE              : "1057161301102698567",
+            PROSPECT                : "1057161308535005204",
+            MYTHIC_FREE_AGENT       : null,                         // not in development server
+            EXPERT_FREE_AGENT       : null,                         // not in development server
+            APPRENTICE_FREE_AGENT   : null,                         // not in development server
+            PROSPECT_FREE_AGENT     : null,                         // not in development server
         }
     } :
     /** @production */
     {
         LEAGUE: {
-            VIEWER: "963665382873391135",
-            LEAGUE: "966901006652833862",
-            INACTIVE: "1060750208746668132",
-            DRAFT_ELIGIBLE: "963568945959419905",
-            FREE_AGENT: "963343035683455007",
-            RESTRICTED_FREE_AGENT: "965817407094530068",
-            INACTIVE_RESERVE: "976541355625025536",
-            CAPTAIN: "963863666464268328",
-            FORMER_PLAYER: "966901052626595860",
+            VIEWER                  : "963665382873391135",
+            LEAGUE                  : "966901006652833862",
+            INACTIVE                : "1060750208746668132",
+            DRAFT_ELIGIBLE          : "963568945959419905",
+            FREE_AGENT              : "963343035683455007",
+            RESTRICTED_FREE_AGENT   : "965817407094530068",
+            INACTIVE_RESERVE        : "976541355625025536",
+            CAPTAIN                 : "963863666464268328",
+            FORMER_PLAYER           : "966901052626595860",
         },
         TIER: {
-            MYTHIC: "967698140612395018",
-            EXPERT: "967697939772354600",
-            APPRENTICE: "973344710540271666",
-            PROSPECT: "966106018796941392",
+            MYTHIC                  : "967698140612395018",
+            EXPERT                  : "967697939772354600",
+            APPRENTICE              : "973344710540271666",
+            PROSPECT                : "966106018796941392",
+            MYTHIC_FREE_AGENT       : "1028478484349206572",
+            EXPERT_FREE_AGENT       : "1028478833055240243",
+            APPRENTICE_FREE_AGENT   : "1028478841611620463",
+            PROSPECT_FREE_AGENT     : "1028478846753837106",
         }
     };
