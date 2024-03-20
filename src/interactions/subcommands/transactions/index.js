a89f597a0bd69eb892bda2f378ff1a9db17d79ae
Lctuff <124016888+Lctuff@users.noreply.github.com>
2024-03-20 10:12:38 -0600

    Fixed shorting the transaction files into subfiles (#67)
    * Shorted The Transaction file into subfiles
    
    * Delete vdc-bot-2-architecture-rewrite directory
    
    * Add files via upload
    
    * Update and rename IR.js to ir.js
    
    * Update and rename unSub.js to unsub.js

--- src/interactions/subcommands/transactions/index.js
+++ src/interactions/subcommands/transactions/index.js
@@ -1,12 +1,54 @@
 const { requestSign, confirmSign } = require(`./sign`);
 const { requestCut, confirmCut } = require(`./cut`);
+const { requestDraftSign, confirmDraftSign } = require("./draftSign");
+const { requestUpdateTier, confirmUpdateTier } = require("./updateTier");
+const { requestRenew, confirmRenew } = require("./renew");
+const { requestSub, confirmSub } = require("./sub");
+const { requestUnsub, confirmUnsub } = require("./unSub");
+const { requestIR, confirmSetIR, confirmRemoveIR } = require("./IR");
+const { requestSwap, confirmSwap } = require("./swap");
+const { requestRetire, confirmRetire } = require("./retire");
 
 module.exports = {
-    sign : {
-        sign: requestSign,
-        confirm: confirmSign
-    },
-    cut: {
-
-    },
-}
+  sign: {
+    sign: requestSign,
+    confirm: confirmSign,
+  },
+  cut: {
+    cut: requestCut,
+    confirm: confirmCut,
+  },
+  draftSign: {
+    draftSign: requestDraftSign,
+    confirm: confirmDraftSign,
+  },
+  updateTier: {
+    updateTier: requestUpdateTier,
+    confirm: confirmUpdateTier,
+  },
+  renew: {
+    renew: requestRenew,
+    confirm: confirmRenew,
+  },
+  sub: {
+    sub: requestSub,
+    confirm: confirmSub,
+  },
+  unsub: {
+    unsub: requestUnsub,
+    confirm: confirmUnsub,
+  },
+  ir: {
+    ir: requestIR,
+    confirmSet: confirmSetIR,
+    confirmRemove: confirmRemoveIR,
+  },
+  swap: {
+    swap: requestSwap,
+    confirm: confirmSwap,
+  },
+  retire: {
+    retire: requestRetire,
+    confirm: confirmRetire,
+  },
+};
