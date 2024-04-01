fc5d6e9654d3f1d5847d79d74f6e477d5640b0a1
Rohan Gupta <75713762+torohangupta@users.noreply.github.com>
2024-04-01 13:36:27 -0500

    Removed dead file

--- src/interactions/commands/draft.js
+++
@@ -1,6 +0,0 @@
-const { Franchise, Player, Team } = require("../../../prisma");
-
-async function draft(tier) {
-  draftTeams = Team.filter((t) => t.tier === tier);
-  draftPlayers = Player.filter((p) => p.tier === tier);
-}
