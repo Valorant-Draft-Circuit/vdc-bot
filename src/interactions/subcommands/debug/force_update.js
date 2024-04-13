aec9d9d451b7d12e5cf62dfc9fe62ccd06655c2f
Rohan Gupta <75713762+torohangupta@users.noreply.github.com>
2024-04-13 11:51:36 -0500

    Force update (#87)
    * Increased lines printed to console by logger
    
    * Added force-update

---
+++ src/interactions/subcommands/debug/force_update.js
@@ -0,0 +1,69 @@
+const { LeagueStatus } = require(`@prisma/client`);
+const { Player, Transaction, Flags, Team } = require(`../../../../prisma`);
+const fs = require(`fs`);
+const { ChatInputCommandInteraction, EmbedBuilder } = require(`discord.js`);
+const { prisma } = require("../../../../prisma/prismadb");
+
+
+async function forceUpdate(/** @type ChatInputCommandInteraction */ interaction) {
+    const { _subcommand, _hoistedOptions } = interaction.options;
+
+    const debugUser = _hoistedOptions[0].user;
+    const leagueStatus = _hoistedOptions.find(o => o.name === `league-status`);
+    const contractStatus = _hoistedOptions.find(o => o.name === `contract-status`);
+    const contractRemaining = _hoistedOptions.find(o => o.name === `contract-remaining`);
+    const team = _hoistedOptions.find(o => o.name === `team`);
+
+    const updatedDataObject = { User: { update: { data: { team: null, Status: { update: {} } } } } };
+    if (leagueStatus !== undefined) updatedDataObject.User.update.data.Status.update.leagueStatus = leagueStatus.value;
+    if (contractStatus !== undefined) {
+        if (contractStatus.value === `999`) updatedDataObject.User.update.data.Status.update.contractStatus = null
+        else updatedDataObject.User.update.data.Status.update.contractStatus = contractStatus.value;
+    }
+    if (contractRemaining !== undefined) {
+        console.log(String(contractRemaining.value) == `999`)
+        if (contractRemaining.value === 999) updatedDataObject.User.update.data.Status.update.contractRemaining = null
+        else updatedDataObject.User.update.data.Status.update.contractRemaining = contractRemaining.value;
+    }
+    if (team !== undefined) {
+        const teamData = await Team.getBy({ name: team.value });
+        updatedDataObject.User.update.data.team = teamData.id;
+    } else delete updatedDataObject.User.update.data.team;
+
+    if (leagueStatus === undefined && contractStatus === undefined && contractRemaining === undefined) delete updatedDataObject.User.update.data.Status;
+
+    if (Object.keys(updatedDataObject.User.update.data).length === 0) return interaction.editReply(`You didn't give any paramaters to update for ${debugUser}`)
+
+    const updatedUser = await prisma.account.update({
+        where: { providerAccountId: debugUser.id },
+        data: updatedDataObject,
+        include: { User: { include: { Status: true, PrimaryRiotAccount: true, Team: true } } }
+    });
+
+    const embed = new EmbedBuilder({
+        author: { name: `Updated User - ${updatedUser.User.name}`, icon_url: updatedUser.User.image },
+        description:
+            `\`  Discord Account \` : <@${updatedUser.providerAccountId}>\n` +
+            `\` Primary Riot IGN \` : [\`${updatedUser.User.PrimaryRiotAccount.riotIGN}\`](https://tracker.gg/valorant/profile/riot/${encodeURIComponent(updatedUser.User.PrimaryRiotAccount.riotIGN)})`
+        ,
+        color: 0xE92929,
+        fields: [
+            {
+                name: `\u200B`,
+                value:
+                    `__**Current User State**__ (After Updates)\n` +
+                    `\`               Team \` : ${updatedUser.User.Team?.name}\n` +
+                    `\`      League Status \` : ${updatedUser.User.Status.leagueStatus}\n` +
+                    `\`    Contract Status \` : ${updatedUser.User.Status.contractStatus}\n` +
+                    `\` Contract Remaining \` : ${updatedUser.User.Status.contractRemaining}`
+                ,
+                inline: false
+            }
+        ],
+        footer: { text: `Valorant Draft Circuit — Update User` }
+    });
+
+    return await interaction.editReply({ embeds: [embed] });
+}
+
+module.exports = { forceUpdate };
