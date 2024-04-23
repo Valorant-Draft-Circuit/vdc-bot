a07b3ae98cb7469c77b9a6a6191fbc6e6fef68c3
Rohan Gupta <75713762+torohangupta@users.noreply.github.com>
2024-04-23 16:54:25 -0500

    Included the require statement for prisma

--- src/interactions/subcommands/transactions/sign.js
+++ src/interactions/subcommands/transactions/sign.js
@@ -5,6 +5,7 @@ const { ChatInputCommandInteraction, GuildMember } = require(`discord.js`);
 const { Franchise, Player, Team, Transaction } = require(`../../../../prisma`);
 const { ROLES, CHANNELS, TransactionsNavigationOptions } = require(`../../../../utils/enums`);
 const { LeagueStatus } = require("@prisma/client");
+const { prisma } = require("../../../../prisma/prismadb");
 
 const emoteregex = /(\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff])/g;
 
