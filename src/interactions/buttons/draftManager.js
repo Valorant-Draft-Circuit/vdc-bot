6eddb738dbadddc3ed50863b963cc70421976327
Rohan Gupta <75713762+torohangupta@users.noreply.github.com>
2024-04-29 02:27:53 -0500

    add draftManager for buttons

---
+++ src/interactions/buttons/draftManager.js
@@ -0,0 +1,20 @@
+const { ButtonInteraction } = require(`discord.js`);
+const { ButtonOptions } = require(`../../../utils/enums`);
+const { cancelDraft, executeDraft } = require("../subcommands/draft/draftPlayer");
+
+module.exports = {
+    id: `draftManager`,
+
+    async execute(/** @type ButtonInteraction */ interaction, args) {
+        await interaction.deferReply({ ephemeral: true }); // defer as early as possible
+
+        switch (Number(args)) {
+            case ButtonOptions.DRAFT_CANCEL: {
+                return await cancelDraft(interaction);
+            }
+            case ButtonOptions.DRAFT_CONFIRM: {
+                return await executeDraft(interaction);
+            }
+        }
+    }
+};
