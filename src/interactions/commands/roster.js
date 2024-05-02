06dda2cd3473f9aeb7c7230ba0bc122b6173d203
Rohan Gupta <75713762+torohangupta@users.noreply.github.com>
2024-05-02 12:30:02 -0500

    S6 preseason bugfixes (#106)
    * bump prisma commit
    
    * only show MMR when flag is set true in controlpanel
    
    * fixed bug that removes league role
    
    * Fixed undefined reference error for emote on announcement

--- src/interactions/commands/roster.js
+++ src/interactions/commands/roster.js
@@ -1,18 +1,10 @@
-const { Team, Franchise } = require(`../../../prisma`);
-const { EmbedBuilder, ApplicationCommand, ChatInputCommandInteraction, DiscordAPIError } = require("discord.js");
+const { Team, Franchise, ControlPanel } = require(`../../../prisma`);
+const { EmbedBuilder, ChatInputCommandInteraction } = require("discord.js");
 
-const { ROLES, PlayerStatusCode } = require("../../../utils/enums");
 const { LeagueStatus, ContractStatus } = require("@prisma/client");
 
 const imagesURL = `https://uni-objects.nyc3.cdn.digitaloceanspaces.com/vdc/team-logos`;
 const sum = (array) => array.reduce((s, v) => s += v == null ? 0 : v, 0);
-
-const teamMMRAllowance = {
-   prospect: 386,
-   apprentice: 538,
-   expert: 716,
-   mythic: 948
-}; // max MMR allowance for teams to "spend" on players
 
 module.exports = {
 
@@ -20,6 +12,10 @@ module.exports = {
 
    async execute(/** @type ChatInputCommandInteraction */ interaction) {
       await interaction.deferReply();
+
+      const showMMR = await ControlPanel.getMMRDisplayState();
+
+
       const { _hoistedOptions } = interaction.options;
 
       // get data from DB
@@ -27,7 +23,13 @@ module.exports = {
       const team = await Team.getBy({ name: teamName });
       const teamRoster = await Team.getRosterBy({ name: teamName });
       const franchise = await Franchise.getBy({ teamName: teamName });
-      console.log(teamRoster)
+      const gmIDs = [
+         franchise.GM?.Accounts.find(a => a.provider == `discord`).providerAccountId,
+         franchise.AGM1?.Accounts.find(a => a.provider == `discord`).providerAccountId,
+         franchise.AGM2?.Accounts.find(a => a.provider == `discord`).providerAccountId
+      ].filter(v => v !== undefined)
+
+      const teamMMRCap = (await ControlPanel.getMMRCaps("TEAM"))[team.tier];
 
       // process db data and organize for display
       const refinedRoster = await refinedRosterData(interaction, teamRoster);
@@ -35,16 +37,16 @@ module.exports = {
       // filter by rostered/IR & suvs and format with Riot ID, Tracker link & add emotes
       const rosteredPlayers = refinedRoster
          .filter(player => (player.leagueStatus === LeagueStatus.SIGNED || player.leagueStatus === LeagueStatus.GENERAL_MANAGER) && player.contractStatus !== ContractStatus.INACTIVE_RESERVE)
-         .map((player) => `${player.captain ? `🪖` : `👤`} | \` ${String(player.mmr).padStart(3)} \` | [${player.riotIDPlain}](${player.trackerURL})`).join(`\n`);
+         .map((player) => `${player.captain ? `🪖` : `👤`} ${showMMR ? `| \` ${String(player.mmr).padStart(3)} \`` : ``} | [${player.riotIDPlain}](${player.trackerURL})`).join(`\n`);
       const inactiveReserve = refinedRoster
          .filter(player => (player.leagueStatus === LeagueStatus.SIGNED || player.leagueStatus === LeagueStatus.GENERAL_MANAGER) && player.contractStatus === ContractStatus.INACTIVE_RESERVE)
-         .map((player) => `🛡️ | \` ${String(player.mmr).padStart(3)} \` | [${player.riotIDPlain}](${player.trackerURL})`).join(`\n`);
+         .map((player) => `🛡️ ${showMMR ? `| \` ${String(player.mmr).padStart(3)} \`` : ``} | [${player.riotIDPlain}](${player.trackerURL})`).join(`\n`);
       const activeSub = refinedRoster
          .filter(player => (player.leagueStatus === LeagueStatus.FREE_AGENT || player.leagueStatus === LeagueStatus.RESTRICTED_FREE_AGENT) && player.contractStatus === ContractStatus.ACTIVE_SUB)
-         .map((player) => `📝 | \` ${String(player.mmr).padStart(3)} \` | [${player.riotIDPlain}](${player.trackerURL})`).join(`\n`);
+         .map((player) => `📝 ${showMMR ? `| \` ${String(player.mmr).padStart(3)} \`` : ``} | [${player.riotIDPlain}](${player.trackerURL})`).join(`\n`);
       const expiringContract = refinedRoster
          .filter(player => (player.leagueStatus === LeagueStatus.SIGNED || player.leagueStatus === LeagueStatus.GENERAL_MANAGER) && player.contractStatus === ContractStatus.SIGNED && player.contractRemaining === 0)
-         .map((player) => `📤  | \` ${String(player.mmr).padStart(3)} \` | [${player.riotIDPlain}](${player.trackerURL})`).join(`\n`);
+         .map((player) => `📤  ${showMMR ? `| \` ${String(player.mmr).padStart(3)} \`` : ``} | [${player.riotIDPlain}](${player.trackerURL})`).join(`\n`);
 
       // add if the type of sub ONLY IF it has players in it
       const description = [];
@@ -55,14 +57,14 @@ module.exports = {
 
       // filter mmr to only include signed players who are not on IR (this ignores FA & RFA)
       const teamMMRArr = refinedRoster
-         .filter(player => player.status === PlayerStatusCode.SIGNED && player.contractStatus !== ContractStatus.INACTIVE_RESERVE)
+         .filter(player => player.leagueStatus === LeagueStatus.SIGNED && player.contractStatus !== ContractStatus.INACTIVE_RESERVE)
          .map((player) => player.mmr);
       const teamMMR = sum(teamMMRArr);
 
       // build and then send the embed confirmation
       const embed = new EmbedBuilder({
          author: { name: `${franchise.name} - ${teamName}`, icon_url: `${imagesURL}/${franchise.Brand.logo}` },
-         description: `\`     Tier \` : ${team.tier}\n\` Team MMR \` : ${teamMMR} / ${teamMMRAllowance[team.tier.toLowerCase()]}`,
+         description: `\`   Managers \` : ${gmIDs.map(gm => `<@${gm}>`)}\n\`       Tier \` : ${team.tier}${showMMR ? `\n\`   Team MMR \` : ${teamMMR} / ${teamMMRCap}` : ``}`,
          color: 0xE92929,
          fields: [
             {
