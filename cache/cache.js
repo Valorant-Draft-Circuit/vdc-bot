75c89349cbf5205b95776dc8adfab2bf17eeab45
Rohan Gupta <75713762+torohangupta@users.noreply.github.com>
2024-05-29 18:33:33 -0500

    Update `cache` to include tier & sort by tier (lowest -> highest)

--- cache/cache.js
+++ cache/cache.js
@@ -1,6 +1,13 @@
 const fs = require('fs');
 const { Franchise } = require(`../prisma`);
 
+// this is needed to correctly sort the tiers
+const tierSortWeights = {
+    PROSPECT: 1,
+    APPRENTICE: 2,
+    EXPERT: 3,
+    MYTHIC: 4
+};
 
 // #####################################################################################
 // #####################################################################################
@@ -30,9 +37,16 @@ async function clearCache() {
 
 /** Query the database to generate the cache files */
 async function generateCache() {
+    // get all active franchises
     const franchiseCache = await Franchise.getAllActive();
+
+    // create cache of all franchises
     const fc = franchiseCache.map((f) => { return { name: f.name, slug: f.slug } });
-    const tc = franchiseCache.map((f) => f.Teams.filter((t) => t.active).map((t) => { return { slug: f.slug, name: t.name } })).flat();
+    const tc = franchiseCache.map((f) => f.Teams.filter((t) => t.active)
+        .map((t) => { return { slug: f.slug, name: t.name, tier: t.tier } }))
+        .flat().sort((a, b) => tierSortWeights[a.tier] - tierSortWeights[b.tier]);
+
+    // build cache (write to internal files)
     fs.writeFileSync(`./cache/franchises.json`, JSON.stringify(fc));
     fs.writeFileSync(`./cache/teams.json`, JSON.stringify(tc));
 }
