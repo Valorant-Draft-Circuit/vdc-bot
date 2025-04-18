4a1e58bf30ff450b84ae9caef526f79acbbbe802
Rohan Gupta <75713762+torohangupta@users.noreply.github.com>
2025-04-18 15:38:17 -0500

    added ability to add/remove accolades to league command

--- src/interactions/commands/league.js
+++ src/interactions/commands/league.js
@@ -1,7 +1,6 @@
 const { ChatInputCommandInteraction } = require(`discord.js`);
-
-const { updateFranchiseManagement,refreshFranchisesChannel } = require(`../subcommands/league`);
-const { CHANNELS } = require("../../../utils/enums");
+const { updateFranchiseManagement, refreshFranchisesChannel, modifyAccolades } = require(`../subcommands/league`);
+const { CHANNELS } = require(`../../../utils/enums`);
 
 
 module.exports = {
@@ -19,6 +18,10 @@ module.exports = {
             case `refresh-franchises-channel`:
                 await refreshFranchisesChannel(interaction);
                 return await interaction.editReply(`The <#${CHANNELS.FRANCHISES}> channel has been updated!`)
+            case `modify-accolades`:
+                return await modifyAccolades(interaction);
+            default:
+                return await interaction.editReply({ content: `That subcommand doesn't exist! Please try again or open a tech ticket!` });
         }
     }
 };
