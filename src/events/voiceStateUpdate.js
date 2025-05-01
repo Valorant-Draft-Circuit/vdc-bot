edc054e3c3cf5f19144f81290d3f8bd784610f46
Rohan Gupta <75713762+torohangupta@users.noreply.github.com>
2025-05-01 14:26:08 -0500

    Prevent users from playing in combines until they have completed the activity check

--- src/events/voiceStateUpdate.js
+++ src/events/voiceStateUpdate.js
@@ -23,7 +23,7 @@ const { ChannelType, BaseClient, VoiceState, EmbedBuilder, ButtonBuilder, Button
 const { CHANNELS, GUILD, ROLES } = require(`../../utils/enums`);
 const { LeagueStatus } = require(`@prisma/client`);
 
-
+const botCommandsChannelID = `966216243986194432`;
 const enableSort = (/true/i).test(process.env.COMBINES_SORT);
 
 const allowedLeagueStatuses = [
@@ -93,6 +93,13 @@ module.exports = {
 		const isAdmin = userRoles.includes(ROLES.OPERATIONS.ADMIN);
 		const isMod = userRoles.includes(ROLES.OPERATIONS.MOD);
 
+		// if they join any combines channels without completing the activity check, disconnect them regardless of GM status
+		if ((joinedCombinesLobbyBool || isInCombinesCategory) && userRoles.includes(ROLES.LEAGUE.INACTIVE)) {
+			sendDM(m, `You have not completed the activity check and cannot partipate in combines until you do so- please complete it by using the \`/active\` command in <#${botCommandsChannelID}> channel!`);
+			logger.log(`ALERT`, `User ${m.user} (\`${m.user.username}\`, \`${m.id}\`) joined a combines channel without first having completed the activity check & have been disconnected`);
+			return await m.voice.disconnect();
+		}
+
 		// if a player joines the combines lobby, verify valid MMR and then move them to the correct channel
 		if (joinedCombinesLobbyBool) {							// else check mmr and status
 			// get player MMR
@@ -108,7 +115,7 @@ module.exports = {
 				return await m.voice.setChannel(channel);
 			} else {											// not a valid MMR or valid status
 				sendDM(m, `There is a problem with your account (invalid MMR, or status) and you cannot join <#${CHANNELS.VC.COMBINES.SORT_CHANNEL}> currently. If you believe this is an error, please open an admin ticket: <#966924427709276160>`);
-				logger.log(`ALERT`, `User ${m.user} (\`${m.user.username}\`) joined <#${sortChannel}> with an invalid MMR (\`${mmr}\`) or invalid status (\`${playerLeagueStatus}\`) & have been disconnected`);
+				logger.log(`ALERT`, `User ${m.user} (\`${m.user.username}\`, \`${m.id}\`) joined <#${sortChannel}> with an invalid MMR (\`${mmr}\`) or invalid status (\`${playerLeagueStatus}\`) & have been disconnected`);
 				return await m.voice.disconnect();
 			}
 		}
@@ -116,7 +123,7 @@ module.exports = {
 		// if the player joins a channel in the commbines category, confirm that they are a scout or a player with a valid MMR
 		if (isInCombinesCategory) {
 			if (isScout || isAdmin || isMod) {					// if the user is a scout, admin or mod, ignore all restrictions
-				return console.log(`User ${m.user} (\`${m.user.username}\`) joined ${newState.channel.name} as a scout/admin/mod. They have been allowed to join`);
+				return console.log(`User ${m.user} (\`${m.user.username}\`, \`${m.id}\`) joined ${newState.channel.name} as a scout/admin/mod. They have been allowed to join`);
 			} else {											// else check mmr and status
 				// get player MMR
 				const mmr = Number(mmrCache.find(mmr => mmr.discordID === m.id)?.mmr);
@@ -129,18 +136,18 @@ module.exports = {
 
 				if (!playerTier) {								// invalid MMR
 					sendDM(m, `You have joined a combines channel, but you do not have a valid MMR. If you believe this is an error, please open an admin ticket: <#966924427709276160>`);
-					logger.log(`ALERT`, `User ${m.user} (\`${m.user.username}\`) joined ${newState.channel} (\`${newState.channel.name}\`) with an invalid MMR (\`${mmr}\`) & have been disconnected`);
+					logger.log(`ALERT`, `User ${m.user} (\`${m.user.username}\`, \`${m.id}\`) joined ${newState.channel} (\`${newState.channel.name}\`) with an invalid MMR (\`${mmr}\`) & have been disconnected`);
 					return await m.voice.disconnect();
 				} else if (!isValidStatus) {					// invalid status
 					sendDM(m, `You have joined a combines channel, but you do not have a valid status. If you believe this is an error, please open an admin ticket: <#966924427709276160>`);
-					logger.log(`ALERT`, `User ${m.user} (\`${m.user.username}\`) joined ${newState.channel} (\`${newState.channel.name}\`) with an invalid status (\`${playerLeagueStatus}\`) & have been disconnected`);
+					logger.log(`ALERT`, `User ${m.user} (\`${m.user.username}\`, \`${m.id}\`) joined ${newState.channel} (\`${newState.channel.name}\`) with an invalid status (\`${playerLeagueStatus}\`) & have been disconnected`);
 					return await m.voice.disconnect();
 				} else if (!isInCorrectTier) {					// invalid tier
 					sendDM(m, `You have joined a combines channel, but are not in the correct tier- I expected to see you in a \`${playerTier}\` lobby! I've moved you to <#${CHANNELS.VC.COMBINES.SORT_CHANNEL}> to be sorted to the right tier, but in the future, please join that channel at the beginning of each combines night! If you believe this is an error, please open an admin ticket: <#966924427709276160>`);
-					logger.log(`ALERT`, `User ${m.user} (\`${m.user.username}\`) joined ${newState.channel} (\`${newState.channel.name}\`)- expected to see them in a \`${playerTier}\` lobby. They have been moved to <#${CHANNELS.VC.COMBINES.SORT_CHANNEL}> to be sorted correctly`);
+					logger.log(`ALERT`, `User ${m.user} (\`${m.user.username}\`, \`${m.id}\`) joined ${newState.channel} (\`${newState.channel.name}\`)- expected to see them in a \`${playerTier}\` lobby. They have been moved to <#${CHANNELS.VC.COMBINES.SORT_CHANNEL}> to be sorted correctly`);
 					return await m.voice.setChannel(CHANNELS.VC.COMBINES.SORT_CHANNEL);
 				} else {										// valid MMR, tier and status
-					return console.log(`User ${m.user} (\`${m.user.username}\`) joined ${m.voice.channel} (${m.voice.channel.name}) with an MMR of \`${mmr}\`, leagueStatus of \`${playerLeagueStatus}\` & tier of \`${playerTier}\``);
+					return console.log(`User ${m.user} (\`${m.user.username}\`, \`${m.id}\`) joined ${m.voice.channel} (${m.voice.channel.name}) with an MMR of \`${mmr}\`, leagueStatus of \`${playerLeagueStatus}\` & tier of \`${playerTier}\``);
 				}
 			}
 		}
