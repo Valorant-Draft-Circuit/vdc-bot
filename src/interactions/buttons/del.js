dcfbc6454fc9d0e6b7e612a5c4e00145df74faf5
Rohan Gupta <75713762+torohangupta@users.noreply.github.com>
2025-05-23 19:19:57 -0500

    to help with testing

--- src/interactions/buttons/del.js
+++ src/interactions/buttons/del.js
@@ -1,13 +1,14 @@
 const { ButtonInteraction } = require(`discord.js`);
 const { ButtonOptions } = require(`../../../utils/enums`);
 const { cancelDraft, executeDraft } = require("../subcommands/draft/draftPlayer");
+const { prisma } = require("../../../prisma/prismadb");
 
 module.exports = {
     id: `del`,
 
     async execute(/** @type ButtonInteraction */ interaction, args) {
         await interaction.deferReply({ ephemeral: true }); // defer as early as possible
-
+        await prisma.mapBans.deleteMany({ where: { matchID: 719 } });
         return await interaction.channel.delete();
     }
 };
