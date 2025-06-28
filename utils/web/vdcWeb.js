3864149677f45cc0f10cdcaa2a3c643ae4be42db
Sooho <sooho.lim4@gmail.com>
2025-06-28 11:52:30 -0700

    fix logging and update example.env

--- utils/web/vdcWeb.js
+++ utils/web/vdcWeb.js
@@ -1,16 +1,15 @@
 // TODO: Put this in vdc-common
 
 async function updateMeilisearchPlayer(playerId) {
-    console.log(`Updating Meilisearch player document for player id: ${playerId}`)
+    logger.log(`DEBUG`, `Updating Meilisearch player document for player id: \`${playerId}\``);
     try {
         const res = await fetch(`https://${process.env.VDC_WEB_URL}/api/meilisearch/player/${playerId}`);
         if (!res.ok) {
-            console.warn("Unable to update Meilisearch player document.");
+            logger.log(`WARN`, `Unable to update Meilisearch player document.`);
         }
     } catch (error) {
-        console.warn("Unable to update Meilisearch player document.", error);
+        logger.log(`WARN`, `Unable to update Meilisearch player document. Error: ${error}`);
     }
-	
 }
 
 module.exports = {
