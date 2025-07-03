3e6bb13c57b92f4cbc65e4c1e5f64b84adbc5bbf
Rohan Gupta <75713762+torohangupta@users.noreply.github.com>
2025-07-02 22:42:45 -0500

    Logger updates `WARN` -> `WARNING`

--- utils/web/vdcWeb.js
+++ utils/web/vdcWeb.js
@@ -5,10 +5,10 @@ async function updateMeilisearchPlayer(playerId) {
     try {
         const res = await fetch(`https://${process.env.VDC_WEB_URL}/api/meilisearch/player/${playerId}`);
         if (!res.ok) {
-            logger.log(`WARN`, `Unable to update Meilisearch player document.`);
+            logger.log(`WARNING`, `Unable to update Meilisearch player document.`);
         }
     } catch (error) {
-        logger.log(`WARN`, `Unable to update Meilisearch player document. Error: ${error}`);
+        logger.log(`WARNING`, `Unable to update Meilisearch player document. Error: ${error}`);
     }
 }
 
