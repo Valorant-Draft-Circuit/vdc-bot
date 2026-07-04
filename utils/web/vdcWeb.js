50cc2baccf2d32824740640c2db7d2250ddf027d
Sooho <sooho.lim4@gmail.com>
2026-07-03 22:47:25 -0700

    meili master key

--- utils/web/vdcWeb.js
+++ utils/web/vdcWeb.js
@@ -3,7 +3,7 @@
 async function updateMeilisearchPlayer(playerId) {
     logger.log(`DEBUG`, `Updating Meilisearch player document for player id: \`${playerId}\``);
     try {
-        const res = await fetch(`https://${process.env.VDC_WEB_URL}/api/internal/meilisearch/player/${playerId}`);
+        const res = await fetch(`https://${process.env.VDC_WEB_URL}/api/meilisearch/player/${playerId}?meiliauth=${process.env.MEILISEARCH_MASTER_KEY}`);
         if (!res.ok) {
             logger.log(`WARNING`, `Unable to update Meilisearch player document.`);
         }
